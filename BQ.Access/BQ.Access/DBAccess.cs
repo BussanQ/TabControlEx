8a068d2dab18f00b0ae9b5a2cbe898517e47d7f9
EnmaAiJie <bussanq@qq.com>
2016-04-20 14:23:53 +0800

    create123123

---
+++ BQ.Access/BQ.Access/DBAccess.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb; 
+using System.Text;
+using System.Data;
+using System.Configuration;
+
+namespace BQ.Access
+{
+    /// <summary>
+    /// OracleClient访问方式
+    /// </summary>
+    public class DBAccess 
+    {
+
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        private static string connStr = string.Empty;
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        private static DBAccess instance = null;
+
+        /// <summary>
+        /// 异步控制对象
+        /// </summary>
+        private static object synObj = new object();
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        public static DBAccess Instance
+        {            
+            get
+            {
+                lock (synObj)
+                {
+                    connStr = ConfigurationManager.ConnectionStrings["Access"].ConnectionString;
+                    if (instance == null)
+                        instance = new DBAccess();                    
+                    return instance;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行查询语句，返回DataSet
+        /// </summary>
+        /// <param name="strSQL">查询语句</param>
+        /// <returns>DataSet</returns>
+        public DataSet Query(string strSQL)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connStr))
+            {
+                DataSet ds = new DataSet();
+                try
+                {
+                    connection.Open();
+                    OleDbDataAdapter command = new OleDbDataAdapter(strSQL, connection);
+                    command.Fill(ds, "ds");
+                }
+                catch (System.Data.OleDb.OleDbException ex)
+                {
+                    Log.WriteLogMessage("BQ.DBAccess", "Query(string strSQL)", ex.Message + strSQL, true);
+                }
+                return ds;
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL语句，返回影响的记录数
+        /// </summary>
+        /// <param name="strSQL">SQL语句</param>
+        /// <returns>影响的记录数</returns>
+        public int ExecuteSql(string strSQL)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand(strSQL, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        return rows;
+                    }
+                    catch (System.Data.OleDb.OleDbException ex)
+                    {
+                        connection.Close();
+                        Log.WriteLogMessage("BQ.DBAccess", "ExecuteSql(string strSQL)", ex.Message + strSQL, true);
+                        return -1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL语句，返回影响的记录数
+        /// </summary>
+        /// <param name="strSQL">SQL语句</param>
+        /// <returns>影响的记录数</returns>
+        public int ExecuteSql(IList<string> sqlList)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connStr))
+            {
+                OleDbTransaction oraTran = connection.BeginTransaction();
+                foreach (string strSQL in sqlList)
+                {
+                    using (OleDbCommand cmd = new OleDbCommand(strSQL, connection, oraTran))
+                    {
+                        try
+                        {
+                            connection.Open();
+                            int rows = cmd.ExecuteNonQuery();
+                        }
+                        catch (System.Data.OleDb.OleDbException ex)
+                        {
+                            oraTran.Rollback();
+                            connection.Close();
+                            Log.WriteLogMessage("BQ.DBAccess", "ExecuteSql(string strSQL)", ex.Message + strSQL, true);
+                            return -1;
+                        }
+                    }
+                }
+                oraTran.Commit();
+            }
+            return sqlList.Count;
+        }        
+        
+    }
+}
---
+++ BQ.Access/BQ.Access/Log.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace BQ.Access
+{
+    /// <summary>
+    /// 日志操作类
+    /// </summary>
+    public class Log
+    {       
+        /// <summary>
+        /// 写日志文件
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="isException">是否是出错信息</param>
+        public static void WriteLogMessage(string message, bool isException)
+        {
+            string logFolderPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.FullName + "\\Log\\";//Directory.GetParent(codeBase.Substring(8)).Parent.FullName + "\\Log\\";
+            string logFilePath = string.Format("{0}BQ_{1}.log", logFolderPath, DateTime.Now.ToString("yyyy_MM_dd"));
+            StreamWriter streamWriter;
+            if (!Directory.Exists(logFolderPath))
+                Directory.CreateDirectory(logFolderPath);
+            //检查日志文件是否存在
+            if (File.Exists(logFilePath))
+                streamWriter = new StreamWriter(File.Open(logFilePath, FileMode.Append, FileAccess.Write, FileShare.Read));
+            else
+                streamWriter = new StreamWriter(File.Open(logFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read));
+            //是否异常日志
+            if (isException)
+                streamWriter.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + " Error : " + message);
+            else
+                streamWriter.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + " Normal : " + message);
+            streamWriter.WriteLine("------------------------------------------------------------------------------");
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="className">类名称</param>
+        /// <param name="methodName">方法名称</param>
+        /// <param name="message">消息</param>
+        /// <param name="isException">是否异常日志</param>
+        public static void WriteLogMessage(string className, string methodName, string message, bool isException)
+        {
+            string logInfo = string.Format("类名称：{0} 方法名称：{1} 消息：{2}", className, methodName, message);
+            WriteLogMessage(logInfo, isException);
+        }
+    }
+}
