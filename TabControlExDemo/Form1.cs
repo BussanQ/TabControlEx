ba48ca2ef1f3886756dafe1991a16e9c76e29b7c
BussanQ <bussanq@qq.com>
2016-04-20 14:44:01 +0800

    add Project

---
+++ TabControlExDemo/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace TabControlExDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControlEx2.IsCloseShow = true;
+            tabControlEx2.TabPages.Add("1");
+        }
+    }
+}
